a7848aed414d09b8431ee3e8668cd7537e40ee73
Gavan Wilhite <gavan@gavanwilhite.com>
2014-01-20 15:50:56 -0800

    added some navigation scripts

---
+++ Assets/Navigation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigation : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftArrow)){
+			CoherentUIView view = GameObject.Find("/DisplayManager").GetComponent<DisplayManager>().FocusedDisplayController.View;
+			//Do back with BrowserView when availiable
+		}
+	}
+}
---
+++ Assets/Qualia/Scripts/SpeedDial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpeedDial : MonoBehaviour {
+	private List<string> urls = new List<string>{
+		"www.google.com",
+		"http://www.chromeexperiments.com/webgl/",
+		"http://youtu.be/Rsf35tugWkg?html5=1&autoplay=1&hd=1",
+		"http://recode.net/",
+		"http://www.script-tutorials.com/demos/372/index.html"
+	};
+	void Start () {
+	
+	}
+	
+	void Update () {
+		for(int i = 0; i < urls.Count; i++){
+			if(Input.GetKey(KeyCode.LeftControl) && Input.GetKey((KeyCode)(49 + i))){
+				GameObject.Find("/DisplayManager").GetComponent<DisplayManager>().FocusedDisplayController.View.Page = urls[i];
+			}
+		}
+	}
+}
