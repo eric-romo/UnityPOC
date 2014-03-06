08a48a09d3444d34c85811a10ac273fd75e2452e
Gavan Wilhite <gavan@gavanwilhite.com>
2014-03-05 21:15:47 -0800

    classic chess set

--- Assets/Qualia/Apps/HoloChess/AndromedaSystem/Scripts/Hologram Effect Helpers/DustAnimator.js
+++
@@ -1,7 +0,0 @@
-var xSpeed : float = 0; 
-var ySpeed : float = 0; 
-function Update() { 
-var offsetX = Time.time * xSpeed; 
-var offsetY = Time.time * ySpeed; 
-renderer.material.mainTextureOffset = Vector2 (offsetX,offsetY); 
-} 
