0a92138d4846885ec6a36beaf17985b539c44c35
Gavan Wilhite <gavan@gavanwilhite.com>
2014-03-06 14:53:57 -0800

    holochess working

--- Assets/Qualia/Apps/HoloChess/Scripts/HoloChessController.cs
+++ Assets/Qualia/Apps/HoloChess/Scripts/HoloChessController.cs
@@ -8,42 +8,60 @@ public class HoloChessController : MonoBehaviour {
 	
 	private GameObject holoChessPrefab;
 	private GameObject holoChess;
-	private GameObject cusor;
+	private GameObject cursor;
 	
-	private delegate void CoherentDelegate();
-	
-	private GameObject whiteQueen;
+	private GameObject[] pieces;
+	private GameObject dragPiece;
 	
 	// Use this for initialization
 	void Start () {
-		cusor = transform.Find("Cursor").gameObject;
+		cursor = transform.Find("Cursor").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButton(0) && whiteQueen != null){
-			Vector3 queenPosition = cusor.transform.position;
-			Debug.Log("queen pos: " + queenPosition);
+		if(Input.GetMouseButtonDown(0) && pieces != null && pieces.Length > 0){
+			foreach(GameObject piece in pieces){
+				GameObject pieceMesh = piece.transform.GetChild(0).gameObject;
+				if(cursor.renderer.bounds.Intersects(pieceMesh.renderer.bounds)){
+					Debug.Log("Grabbing piece: " + pieceMesh.name);
+					dragPiece = piece;
+				}
+			}
+		}
+		
+		if(Input.GetMouseButtonUp(0)){
+			dragPiece = null;
+		}
+		
+		if(Input.GetMouseButton(0) && dragPiece != null){
+			Vector3 piecePosition = cursor.transform.position;
+			Debug.Log("dragPiece pos: " + piecePosition);
 			
-			Vector3 queenLocalPosition = whiteQueen.transform.localPosition;
-			whiteQueen.transform.position = queenPosition;
+			Vector3 pieceLocalPosition = dragPiece.transform.localPosition;
+			dragPiece.transform.position = piecePosition;
 			
-			queenLocalPosition.x = whiteQueen.transform.localPosition.x;
-			queenLocalPosition.z = whiteQueen.transform.localPosition.z;
+			pieceLocalPosition.x = dragPiece.transform.localPosition.x;
+			pieceLocalPosition.z = dragPiece.transform.localPosition.z;
 			
-			whiteQueen.transform.localPosition = queenLocalPosition;
+			dragPiece.transform.localPosition = pieceLocalPosition;
 		}
 	}
 	
 	public void Init(GameObject holoChessPrefab){
 		this.holoChessPrefab = holoChessPrefab;
-		GetComponent<DisplayController>().View.View.BindCall("Play", (Action)(ShowBoard));
+		GetComponent<DisplayController>().View.View.BindCall("Play", (Action)(OnPlay));
+		GetComponent<DisplayController>().View.View.BindCall("NewGame", (Action)(OnNewGame));
 		//PlayHoloChess();
 	
 	}
 	
-	public void ShowBoard(){
+	private void OnPlay(){
 		Debug.Log("Playing!");
+		ShowBoard(true);
+	}
+	
+	public void ShowBoard(bool animate = true){
 		holoChess = GameObject.Instantiate(holoChessPrefab) as GameObject;
 		holoChess.name = "HoloChess";
 		holoChess.transform.localPosition = new Vector3(-0.15f, 1.26f, -0.27f);
@@ -53,25 +71,36 @@ public class HoloChessController : MonoBehaviour {
 		holoChess.transform.Find("ChessBoard/BoardTop").gameObject.renderer.sharedMaterial.renderQueue = 2999;
 		holoChess.transform.Find("ChessBoard/BoardBottom").gameObject.renderer.sharedMaterial.renderQueue = 2999;
 		
-		Sequence showSequence = new Sequence();
-		showSequence.AppendInterval(0.01f);
-		showSequence.AppendCallback( () => holoChess.SetActive(false));
-		showSequence.AppendInterval(0.2f);
-		showSequence.AppendCallback( () => holoChess.SetActive(true));
-		showSequence.AppendInterval(0.01f);
-		showSequence.AppendCallback( () => holoChess.SetActive(false));
-		showSequence.AppendInterval(0.05f);
-		showSequence.AppendCallback( () => holoChess.SetActive(true));
-		showSequence.AppendInterval(0.01f);
-		showSequence.AppendCallback(NewGame);
-		showSequence.Play();
+		
+		if(animate){
+			Sequence showSequence = new Sequence();
+			showSequence.AppendInterval(0.01f);
+			showSequence.AppendCallback( () => holoChess.SetActive(false));
+			showSequence.AppendInterval(0.2f);
+			showSequence.AppendCallback( () => holoChess.SetActive(true));
+			showSequence.AppendInterval(0.01f);
+			showSequence.AppendCallback( () => holoChess.SetActive(false));
+			showSequence.AppendInterval(0.05f);
+			showSequence.AppendCallback( () => holoChess.SetActive(true));
+			showSequence.AppendInterval(0.01f);
+			showSequence.AppendCallback(FindAllPices);
+			showSequence.Play();
+		} else {
+			holoChess.SetActive(true);
+			FindAllPices();
+		}
 	}
 	
-	public void NewGame(){
-		whiteQueen = GameObject.Find("ChessPieceQueenWhite").gameObject;
+	public void OnNewGame(){
+		GameObject.Destroy(holoChess);
+		Sequence newGameSequence = new Sequence();
+		newGameSequence.AppendInterval(0.01f);
+		newGameSequence.AppendCallback( () => ShowBoard(false) );
+		newGameSequence.Play();
+		pieces =  null;
 	}
 	
 	private void FindAllPices(){
-		
+		pieces = GameObject.FindGameObjectsWithTag("Draggable");
 	}
 }
