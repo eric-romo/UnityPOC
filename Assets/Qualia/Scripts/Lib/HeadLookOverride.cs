a982e8a7a92739ed485a1c1095e1744a5031b6df
Gavan Wilhite <gavan@gavanwilhite.com>
2014-03-10 17:26:58 -0700

    added MP scene

---
+++ Assets/Qualia/Scripts/Lib/HeadLookOverride.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadLookOverride : MonoBehaviour {
+
+	private GameObject forwardDirection;
+	
+	public float RotationSpeed = 50.0f;
+	// Use this for initialization
+	void Start () {
+		forwardDirection = GameObject.Find("ForwardDirection");
+		OVRCameraController ovrCameraController = FindObjectOfType(typeof(OVRCameraController)) as OVRCameraController;
+		ovrCameraController.FollowOrientation = forwardDirection.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float friRotationSpeed = RotationSpeed * Time.deltaTime;
+		
+		if(Input.GetKey(KeyCode.I)){
+			forwardDirection.transform.Rotate(-friRotationSpeed, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.K)){
+			forwardDirection.transform.Rotate(friRotationSpeed, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.J)){
+			forwardDirection.transform.Rotate(0, -friRotationSpeed, 0);
+		}
+		if(Input.GetKey(KeyCode.L)){
+			forwardDirection.transform.Rotate(0, friRotationSpeed, 0);
+		}
+		
+		Vector3 eulerAngles = forwardDirection.transform.eulerAngles;
+		eulerAngles.z = 0;
+		forwardDirection.transform.eulerAngles = eulerAngles;
+	}
+}
